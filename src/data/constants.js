d112eb2595fd5f197155b5c59da8f9d1f220a270
CodewithBilal <143596179+CodewithBilal@users.noreply.github.com>
2024-01-10 23:33:13 +0500

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,358 @@
+//import PHP from '../../images/php.png'
+export const Bio = {
+  name: "Bilal Ejaz",
+  roles: [
+    "Software Engineer",
+    "Web Developer",
+    "Data Scientist",
+   
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/CodewithBilal",
+  resume:
+    "https://drive.google.com/file/d/1lKnCcWRibfsJxAJBGNDxY2mAypGI768Z/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/bilal-ejaz-se/",
+  //twitter: "https://twitter.com/RishavChanda",
+  //insta: "https://www.instagram.com/rishav_chanda/",
+  //facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: "https://www.php.net//images/logos/php-logo.svg",
+      },
+      {
+        name: "Laravel",
+        image:"https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/logomark-orange@2x.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "	https://logowik.com/content/uploads/images/figma.jpg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "	https://knowledge.tech/assets/i/KnowledgeImages/logo.png",
+    role: "Data Science with Machine Learning Tranee",
+    company: "Knowledge Streams",
+    date: "Nov 2023 - Jan 2023",
+    desc: "Attained expertise in diverse Data science and Machine learning tools.Applied theoretical concepts effectively in hands-on projects, acquiring valuable practical experience",
+    skills: [
+      "EDA",
+      "Machine Learning",
+      "Data Wrangling",
+      "Critical thinking",
+      
+    ],
+    //doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    //img: "https://media.licdn.com/dms/image/C560BAQEeWo06tfd…eta&t=Y3nr7QPzd7gBxAz-KwtUElkaNXnscM8X4A2Oq2h7aac",
+    role: "Laravel Developer Intern",
+    company: "Devolix",
+    date: "Aug 2023 - Oct 2023",
+    desc: "Collaborated with cross-functional teams, including front-end and back-end developers, to deliver high-quality web solutions on time.Identified and resolved complex bugs and issues within the web application",
+    skills: [
+      "Laravel",
+      "PHP",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    //doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://ucp.edu.pk/static/themes/university/images/faviconucp.png",
+    school: "University of Central Punjab",
+    date: "Oct 2019 - July 2023",
+    grade: "3.22/4 CGPA",
+    desc: "I did my Bachelors in Software Engineering from University of Cental Punjab.I gained my knowledge in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Freelance Force at UCP, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Software Engineering",
+  },
+  {
+    id: 1,
+    img: "https://pgc.edu/wp-content/themes/pgc-new/img/PGCLogo.svg",
+    school: "Punjab Group of Colleges",
+    date: "Aug 2017 - Sep 2019",
+    grade: "A",
+    desc: "I studied FSC pre engineering from Punjab group of Colleges with A Grade.",
+    degree: "FSC Pre Engineering",
+  },
+  {
+    id: 2,
+    //img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Usman Science School",
+    date: "Apr 2015 - Apr 2017",
+    grade: "A",
+    //desc: "I studied FSC pre engineering from Punjab group of Colleges with A Grade.",
+    degree: "Matric",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Hospital Management System",
+    date: "Jan 2022 - Feb 2022",
+    description:
+      "Designed a responsive web application by using visual studio code tool and used Html, CSS and JavaScript for frontend and Php and MySQL for backend. ",
+    image:
+      "https://images.pexels.com/photos/518244/pexels-photo-518244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "PHP",
+      "MySQL",
+     
+    ],
+    category: "web app",
+    //github: "https://github.com/rishavchanda/Trackify",
+    //webapp: "https://trackify.duckdns.org",
+  },
+  {
+    id: 0,
+    title: "Cv/Resume Creator",
+    date: "Dec 2022 - Jan 2023",
+    description:
+      "Designed a responsive web application by using visual studio code tool and used Reactjs and CSS for frontend and Php and laravel and MySQL for backend.",
+    image:
+      "https://images.pexels.com/photos/5598289/pexels-photo-5598289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["React Js", "Laravel", "MySQl"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+    member: [
+      {
+        name: "Ali Sarwar",
+        //img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/mian-ali-sarwar-407ba519b/",
+        //github: "https://github.com/rishavchanda/",
+      },
+      
+    ],
+  },
+  {
+    id: 2,
+    title: "Liver Patient Detection",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "	Performed different algorithms related to machine learning on dataset attributes to get the best accuracy and boost its scalability, reliability and efficiency.",
+    image:
+      "https://images.pexels.com/photos/6476587/pexels-photo-6476587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["Python","Random Forest"],
+    category: "machine learning",
+    //github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
+    //webapp: "https://brain-tumor.netlify.app/",
+    // member: [
+    //   {
+    //     name: "Rishav Chanda",
+    //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+    //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //     github: "https://github.com/rishavchanda/",
+    //   },
+  },
+  {
+    id: 3,
+    title: "Womestige",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "Womestige is an android based servicing application which provide services like cooking, handicraft and laundry etc",
+    image:
+      "https://images.pexels.com/photos/6146643/pexels-photo-6146643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["JAVA", "XML", "Firebase"],
+    category: "android app",
+    //github: "https://github.com/rishavchanda/Buckoid-Android-App",
+    //webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  // {
+  //   id: 10,
+  //   title: "Job Finding App",
+  //   date: "Jun 2023 - Jul 2023",
+  //   description:
+  //     "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+  //   image:
+  //     "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+  //   tags: ["React Native", "JavaScript", "Axios"],
+  //   category: "android app",
+  //   github: "https://github.com/rishavchanda/Job-finder-App",
+  //   webapp: "https://github.com/rishavchanda/Job-finder-App",
+  // },
+  
+  {
+    id: 8,
+    title: "IHME (Institute for Health Metrics and Evaluation)",
+    date: "Jan 2023",
+    description:
+      "Analyzed which area is affected more and also identify which gender is affected more from different diseases in previous years.",
+    image:
+      "https://images.pexels.com/photos/806427/pexels-photo-806427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["Python", "Matplotlib", "Seaborn", "Numpy", "Pandas"],
+    category: "machine learning",
+    //github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    //webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+  {
+    id: 1,
+    title: "Music Recomendation System",
+    date: "Jan 2023",
+    description:
+      "I did a project on music recomendation system by using NearestNeighbors.",
+    image:"https://images.pexels.com/photos/18452255/pexels-photo-18452255/free-photo-of-paper-sheets-with-music-notes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["Python", "NearestNeighbors", "Seaborn", "Numpy", "Pandas"],
+    category: "machine learning",
+    github: "https://github.com/CodewithBilal/Python/blob/main/Machine%20learning/music%20(1).ipynb",
+    //webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+  {
+    id: 4,
+    title: "House Prices",
+    date: "Jan 2023",
+    description:
+      "I did a project on House Prices by using SLR.",
+    image:
+      "	https://cdn-icons-png.flaticon.com/512/86/86253.png",
+    tags: ["Python", "SLR", "Seaborn", "Numpy", "Pandas"],
+    category: "machine learning",
+    github: "https://github.com/CodewithBilal/Python/blob/main/Machine%20learning/LSR_notebook.ipynb",
+    //webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+  {
+    id: 5,
+    title: "Pulsar Stars",
+    date: "Jan 2023",
+    description:
+      "I did a project on Pulsar Stars by using SVM.",
+    image:
+      "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["Python", "SVM", "Seaborn", "Numpy", "Pandas"],
+    category: "machine learning",
+    github: "https://github.com/CodewithBilal/Python/blob/main/Machine%20learning/SVM.ipynb",
+    //webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+  {
+    id: 6,
+    title: "Energy Consumption",
+    date: "Jan 2023",
+    description:
+      "I did a project on solar energy consumotion by using a buliding dataset and also predict future energy consumption.",
+    image:
+      "https://images.pexels.com/photos/9799994/pexels-photo-9799994.jpeg",
+    tags: ["Python", "Linear Regression","Rain Forest","LSTM", "Seaborn", "Numpy", "Pandas"],
+    category: "machine learning",
+    //github: "https://github.com/CodewithBilal/Python/blob/main/Machine%20learning/SVM.ipynb",
+    //webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
